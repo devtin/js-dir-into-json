00bf4cf7d842f31bed4eb0626ea7970aa844e654
Martin Rafael Gonzalez <tin@devtin.io>
2020-06-07 14:41:59 -0400

    fix!: remove esm support

--- src/js-dir-into-json.js
+++ src/js-dir-into-json.js
@@ -2,8 +2,9 @@ import { deepListDir } from 'deep-list-dir'
 import { fileListIntoJson } from './lib/file-list-into-json.js'
 import path from 'path'
 
-/* eslint-disable-next-line */
-require = require('esm')(module)
+export const settings = {
+  fileLoader: require
+}
 
 export async function jsDirIntoJson (directory, { extensions = ['*.js', '*.json'], fileLoader = settings.fileLoader } = {}) {
   return fileListIntoJson(await deepListDir(path.resolve(process.cwd(), directory), { pattern: extensions }), { fileLoader, base: directory })
