9545d8a07e0cd2cea0cd34e5fbcc1b431c453477
Martin Rafael Gonzalez <tin@devtin.io>
2020-06-07 14:41:51 -0400

    feat: directory base to process.cwd()

--- src/js-dir-into-json.js
+++ src/js-dir-into-json.js
@@ -1,9 +1,10 @@
 import { deepListDir } from 'deep-list-dir'
 import { fileListIntoJson } from './lib/file-list-into-json.js'
+import path from 'path'
 
 /* eslint-disable-next-line */
 require = require('esm')(module)
 
-export async function jsDirIntoJson (directory, { extensions = ['*.js', '*.json'], fileLoader } = {}) {
-  return fileListIntoJson(await deepListDir(directory, { pattern: extensions }), { fileLoader, base: directory })
+export async function jsDirIntoJson (directory, { extensions = ['*.js', '*.json'], fileLoader = settings.fileLoader } = {}) {
+  return fileListIntoJson(await deepListDir(path.resolve(process.cwd(), directory), { pattern: extensions }), { fileLoader, base: directory })
 }
